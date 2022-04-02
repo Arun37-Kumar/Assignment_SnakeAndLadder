58cd7e0350eef620bcf7d957353697d5ca90c9a9
Arun37-Kumar <arun007624@gmail.com>
2022-04-02 14:49:37 +0530

    commit

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -14,6 +14,11 @@ namespace SnakeAndLadder
 
             int PLAYER_ONE = 1;
             int starPosition = 0;
+
+            Random random = new Random();
+            int dieRoll = random.Next(1,7);
+            Console.WriteLine("Random Number : {0}",dieRoll);
+            Console.ReadLine();
         }
     }
 }
