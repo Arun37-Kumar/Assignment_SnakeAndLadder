a54ee790698cf21efb1f1ff7ff386ce6370ff7c3
Arun37-Kumar <arun007624@gmail.com>
2022-04-02 15:10:23 +0530

    UC3 updated

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -12,12 +12,46 @@ namespace SnakeAndLadder
         {
             Console.WriteLine("Welcome to Snake and Ladder Game!");
 
-            int PLAYER_ONE = 1;
-            int starPosition = 0;
+            //Constants
+            const int PLAYER_ONE = 1;
+            const int NO_PLAY = 0;
+            const int LADDER = 1;
+            const int SNAKE = 2;
 
+            //variables
+            int starPosition = 0;
+            int position = 0;
+            
             Random random = new Random();
+            int dieOption = random.Next(0,2);
             int dieRoll = random.Next(1,7);
             Console.WriteLine("Random Number : {0}",dieRoll);
+            Console.WriteLine("Die Option : {0}", dieOption);
+
+            switch (dieOption)
+            {
+                case NO_PLAY:
+                    position = 0;
+                    break;
+                case LADDER:
+                    position += dieRoll;
+                    Console.WriteLine("Position moved to : {0}",position);
+                    break;
+                case SNAKE:
+                    position -= dieRoll;
+                    if(position < 0)
+                    {
+                        position = 0;
+                        Console.WriteLine("Position moved to : {0}", position);
+                        
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid move");
+                    break;
+
+            }
+
             Console.ReadLine();
         }
     }
